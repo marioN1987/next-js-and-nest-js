43b112ba1a43c68cb24f88feb6b6b1b5891fc8e0
Marios Nikolaou <marios.nikolaou@chrisons.com>
2025-12-13 00:52:32 +0200

    fix home page

--- app-frontend/app/page.tsx
+++ app-frontend/app/page.tsx
@@ -2,8 +2,8 @@ import TrendingNow from "@/app/trending-now/trendingNow";
 
 export default function Home() {
   return (
-    <>
-      <main>{<TrendingNow />}</main>
-    </>
+    <main>
+      <TrendingNow />
+    </main>
   );
 }
