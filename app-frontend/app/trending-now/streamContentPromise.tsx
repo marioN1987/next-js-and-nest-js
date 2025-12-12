e90528dbbc6ced1d1bfac5d82c590568825b9307
Marios Nikolaou <marios.nikolaou@chrisons.com>
2025-12-13 00:53:14 +0200

    change filename

---
+++ app-frontend/app/trending-now/streamContentPromise.tsx
@@ -0,0 +1,9 @@
+import { fetchStreamingContent } from "@/utils/http";
+import { StreamItems } from "@/components/stream-trends-modal/streamItems";
+
+export default async function StreamContentPromise() {
+  //For <Suspense fallback="…"> to activate, it must receive an unresolved Promise from inside the component tree.
+  const items = await fetchStreamingContent();
+
+  return <StreamItems items={items} />;
+}
