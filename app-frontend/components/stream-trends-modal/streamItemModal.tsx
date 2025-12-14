d23ad0f07c742a72937737df892c50f8b66d50d6
Marios Nikolaou <marios.nikolaou@chrisons.com>
2025-12-15 00:06:59 +0200

    create separate comp for youtube video and update watch progress on video pause or end

--- app-frontend/components/stream-trends-modal/streamItemModal.tsx
+++ app-frontend/components/stream-trends-modal/streamItemModal.tsx
@@ -1,38 +1,46 @@
-import { IStreamingContent } from "@/types/IStreamingContent.interface";
 import Modal from "../Modal";
-import styles from "./streamItemModal.module.css";
+import YouTubeVideo from "./youTubeVideo";
+import { IStreamItemModalProps } from "@/types/IStreamItemModalProps.interface";
+import { useWatchHistory } from "@/hooks/useWatchHistory";
+import { useRef } from "react";
 
 export default function StreamItemModal({
   selected,
   onCloseClick,
-}: {
-  selected: IStreamingContent | null;
-  onCloseClick: () => void;
-}) {
-  let embedUrl = null;
-  const matchYoutubeUrl = selected?.video_url.match(
-    /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/
-  );
+}: IStreamItemModalProps) {
+  const { addOrUpdateItem } = useWatchHistory();
+  const progressRef = useRef<number>(0);
 
-  if (!matchYoutubeUrl) {
-    console.log("Invalid YouTube URL");
-    embedUrl = selected?.video_url;
-  } else {
-    embedUrl = `https://www.youtube.com/embed/${matchYoutubeUrl[1]}`;
-  }
+  const match = selected?.video_url.match(
+    /(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+  );
+  const videoId = match ? match[1] : null;
+
+  const handleWatchProgress = (watch_progress: number) => {
+    if (!selected?.id || !selected?.title) return;
+
+    const progress = Math.min(Math.max(+watch_progress.toFixed(2), 0), 100);
+
+    if (progress !== progressRef.current) {
+      progressRef.current = progress;
+      addOrUpdateItem(
+        {
+          id: selected.id,
+          title: selected.title,
+          thumbnail_url: selected.thumbnail_url,
+        },
+        progress
+      );
+    }
+  };
 
   return (
     <Modal open={!!selected} onClose={onCloseClick}>
       <h2 className="text-xl font-semibold mb-4">{selected?.title}</h2>
-      <iframe
-        width="auto"
-        height="auto"
-        className={styles.streamIframe}
-        src={embedUrl}
-        title={selected?.title}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
+
+      {selected && videoId && (
+        <YouTubeVideo videoId={videoId} onProgress={handleWatchProgress} />
+      )}
 
       <p>
         <span className="font-bold">Description:</span>
@@ -51,7 +59,7 @@ export default function StreamItemModal({
       </p>
 
       <button
-        aria-label="Close"
+        aria-label="Close Modal"
         onClick={onCloseClick}
         className="px-3 py-2 bg-gray-800 text-white rounded-lg cursor-pointer mt-4"
       >
---
+++ app-frontend/components/stream-trends-modal/youTubeVideo.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { IYouTubeModalProps } from "@/types/IYouTubeModalProps.interface";
+import { useEffect, useRef } from "react";
+
+export default function YouTubeVideo({
+  videoId,
+  onProgress,
+}: IYouTubeModalProps) {
+  const playerRef = useRef<any>(null);
+
+  useEffect(() => {
+    if (!videoId) return;
+
+    // Load YouTube API script if not already loaded
+    if (!(window as any).YT) {
+      const tag = document.createElement("script");
+      tag.src = "https://www.youtube.com/iframe_api";
+      document.body.appendChild(tag);
+    }
+
+    const initPlayer = () => {
+      // Create player instance
+      playerRef.current = new (window as any).YT.Player("yt-player", {
+        videoId,
+        events: {
+          onReady: () => {
+            console.log("YT Player ready!");
+          },
+          onStateChange: (event: any) => {
+            // Only call getCurrentTime if the player is fully ready
+            if (
+              event.data === (window as any).YT.PlayerState.PAUSED ||
+              event.data === (window as any).YT.PlayerState.ENDED
+            ) {
+              const player = playerRef.current;
+              if (player && typeof player.getCurrentTime === "function") {
+                const current = player.getCurrentTime();
+                const duration = player.getDuration();
+                const progressPercent = (current / duration) * 100;
+                if (onProgress) onProgress(progressPercent);
+              }
+            }
+          },
+        },
+      });
+    };
+
+    if ((window as any).YT && (window as any).YT.Player) {
+      initPlayer();
+    } else {
+      (window as any).onYouTubeIframeAPIReady = initPlayer;
+    }
+
+    return () => {
+      if (
+        playerRef.current &&
+        typeof playerRef.current.destroy === "function"
+      ) {
+        playerRef.current.destroy();
+        playerRef.current = null;
+      }
+    };
+  }, [videoId, onProgress]);
+
+  return <div id="yt-player" className="w-full aspect-video" />;
+}
