c73fa4576ed4581c884e68512d80b436f68b8882
Marios Nikolaou <marios.nikolaou@chrisons.com>
2025-12-15 00:01:44 +0200

    create header module css and comp

---
+++ app-frontend/components/header/header.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Link from "next/link";
+import { AuthContext } from "@/context/AuthContext";
+import { useContext } from "react";
+import styles from "./Header.module.css";
+import { usePathname } from "next/navigation";
+
+export default function Header() {
+  const { isAdmin, guestEmail } = useContext(AuthContext);
+
+  const useParams = usePathname();
+
+  const greetingText = () => {
+    let greeting = null;
+    if (isAdmin) {
+      greeting = "admin";
+    } else if (guestEmail) {
+      greeting = guestEmail;
+    }
+
+    if (!greeting) {
+      return null;
+    }
+
+    return (
+      <div className="greeting relative mt-4">
+        <p className="font-italic text-white">Hello {greeting}</p>
+      </div>
+    );
+  };
+
+  return (
+    <header className="text-center py-5 relative bg-black h-[500px]">
+      {/* make image darker with overlay */}
+      <div className="absolute inset-0 bg-black/50" />
+
+      <Link href="/" className={styles.logo}>
+        ZenithFlix
+      </Link>
+
+      <div className="flex flex-row md:justify-center flex-wrap mt-4 md:mt-0">
+        <div className="right-section absolute text-right right-4 md:right-10 z-11">
+          <div className="login-link">
+            <Link
+              href={`${isAdmin || guestEmail ? "/logout" : "/login"}`}
+              type="button"
+              className="text-fg-brand bg-neutral-primary border border-white text-white hover:bg-white hover:border-cyan-100 hover:text-black 
+              transition delay-50 cursor-pointer duration-300 ease-in-out font-medium leading-5 rounded-base text-sm px-4 py-2 focus:outline-none"
+            >
+              {isAdmin || guestEmail ? "Logout" : "Login"}
+            </Link>
+          </div>
+          {greetingText()}
+        </div>
+
+        {/* hide in admin page, admin link */}
+        {isAdmin && useParams !== "/admin" && (
+          <div className="z-12 ml-4">
+            <Link
+              href="/admin"
+              type="button"
+              className="text-fg-brand bg-neutral-primary border border-white text-white hover:bg-white hover:border-cyan-100 hover:text-black 
+                transition delay-50 cursor-pointer duration-300 ease-in-out font-medium leading-5 rounded-base text-sm px-4 py-2 focus:outline-none"
+            >
+              Admin
+            </Link>
+          </div>
+        )}
+      </div>
+
+      <h1 className="text-3xl font-bold tracking-tight text-heading md:text-4xl lg:text-5xl absolute text-white z-10 absolute inset-0 flex items-center justify-center">
+        Welcome to ZenithFlix is a next-generation AI-driven streaming platform
+      </h1>
+    </header>
+  );
+}
