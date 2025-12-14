3940edc1ec2383c9ebf8a35e53e980ee087516e8
Marios Nikolaou <marios.nikolaou@chrisons.com>
2025-12-15 00:11:18 +0200

    add most watched comp in home comp

--- app-frontend/app/page.tsx
+++ app-frontend/app/page.tsx
@@ -1,8 +1,10 @@
 import TrendingNow from "@/app/trending-now/trendingNow";
+import MostWatched from "./most-watched/page";
 
 export default function Home() {
   return (
     <main>
+      <MostWatched />
       <TrendingNow />
     </main>
   );
