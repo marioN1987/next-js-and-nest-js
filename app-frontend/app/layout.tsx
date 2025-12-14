46ea531a38ef0388c4ad98437abd25a7856b2b22
Marios Nikolaou <marios.nikolaou@chrisons.com>
2025-12-15 00:07:36 +0200

    fix import

--- app-frontend/app/layout.tsx
+++ app-frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
+import Header from "@/components/header/header";
 import { AuthProvider } from "@/context/AuthProvider";
 
 const geistSans = Geist({
