1239e63c95e59ff6cca005b5d4eafe31d71602f8
Marios Nikolaou <marios.nikolaou@chrisons.com>
2025-12-15 00:14:18 +0200

    add more methods in helper

--- app-frontend/helper/helperMethods.ts
+++ app-frontend/helper/helperMethods.ts
@@ -1,4 +1,5 @@
-import { IStreamingFormErrors } from "@/types/IStreamingFormErrors.interface";
+import { IStreamingFormErrors } from "@/types/IStreamingFormErrorsPropr.interface";
+import { IWatchHistoryProps } from "@/types/IWatchHistoryProps.interface";
 
 export function extractErrMessage(list: IStreamingFormErrors[], colName: string) {
   return list.find((obj: IStreamingFormErrors) => obj.colName === colName)
@@ -16,4 +17,22 @@ export function isValidUrl(url: string): boolean {
 
 export function isValidEmail(email: string) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+export function getTop5MostWatched(
+  history: IWatchHistoryProps[]
+): IWatchHistoryProps[] {
+  return [...history]
+    .filter(item => typeof item.progress === "number")
+    .sort((a, b) => b.progress - a.progress)
+    .slice(0, 5);
+}
+
+const WATCH_HISTORY_KEY = "watch_history";
+export function getWatchHistory() {
+    const stored = localStorage.getItem(WATCH_HISTORY_KEY);
+
+    if (!stored) return;
+
+    return JSON.parse(stored) as IWatchHistoryProps[];
 }
