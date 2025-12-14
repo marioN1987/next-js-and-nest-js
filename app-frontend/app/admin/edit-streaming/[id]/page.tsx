e3e2616b9dd1c5040ec9c8c86fe7b505b3583211
Marios Nikolaou <marios.nikolaou@chrisons.com>
2025-12-15 00:12:45 +0200

    add/edit streaming

---
+++ app-frontend/app/admin/edit-streaming/[id]/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { editStreamFormAction } from "@/actions/streamingFormAction";
+import GenericForm from "@/components/genericForm";
+import { IStreamingContentProps } from "@/types/IStreamingContentProps.interface";
+import { getStreamById } from "@/utils/http";
+import { useParams } from "next/navigation";
+import { useActionState, useEffect, useState } from "react";
+
+export default function EditStreaming() {
+  const params = useParams();
+  const streamId = params?.id;
+
+  const [selectedStream, setSelectedStream] =
+    useState<IStreamingContentProps>();
+
+  useEffect(() => {
+    (async () => {
+      const streamJson = await getStreamById(streamId);
+      setSelectedStream(streamJson);
+    })();
+  }, [streamId]);
+
+  const [formState, formAction, isPending] = useActionState(
+    editStreamFormAction,
+    {
+      success: false,
+      errors: null,
+      errMessage: null,
+      enteredValues: null,
+    }
+  );
+
+  formState.enteredValues = selectedStream as IStreamingContentProps;
+
+  return (
+    <div className="w-[50%] mx-auto rounded px-8 pt-6 pb-8 mb-4">
+      <h2 className="text-4xl font-bold text-heading text-center mb-6">
+        Edit streaming content
+      </h2>
+
+      {formState?.success === true && <p>Stream updated</p>}
+
+      {formState?.success === false && formState.errMessage && (
+        <p>{formState.errMessage}</p>
+      )}
+
+      {/* don't render until stream id selected */}
+      {selectedStream && (
+        <GenericForm
+          isEditForm={true}
+          formAction={formAction}
+          formState={formState}
+          isPending={isPending}
+        />
+      )}
+    </div>
+  );
+}
