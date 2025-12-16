1eba933209c1d61ab671ce2d06f2fca890ef5d98
Marios Nikolaou <marios.nikolaou@chrisons.com>
2025-12-16 13:22:08 +0200

    comments removed

--- app-backend/src/users/module/users.module.ts
+++ app-backend/src/users/module/users.module.ts
@@ -5,9 +5,7 @@ import { APP_GUARD } from '@nestjs/core';
 import { MockJwtAuthGuard } from '../../auth/mockJwtAuthGuard';
 
 @Module({
-  //imports:[TypeOrmModule.forFeature([StreamingContentEntity])],
   controllers: [UsersController],
   providers: [UsersService, { provide: APP_GUARD, useClass: MockJwtAuthGuard}],
-  //exports: [TypeOrmModule]
 })
 export class UsersModule {}
